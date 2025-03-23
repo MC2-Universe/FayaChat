dfbab875dd8ce3cfea7213ad5cb45666c3f25f06
Joel Serino <thinq4yourself@users.noreply.github.com>
2025-03-23 04:21:40 +0000

    first faya commit

--- src/lib/components/chat/Settings/Connections.svelte
+++ src/lib/components/chat/Settings/Connections.svelte
@@ -127,11 +127,9 @@
 
 					<div class="my-1.5">
 						<div class="text-xs text-gray-500">
-							{$i18n.t('Connect to your own OpenAI compatible API endpoints.')}
+							Connect to custom OpenAI compatible API endpoints
 							<br />
-							{$i18n.t(
-								'CORS must be properly configured by the provider to allow requests from Open WebUI.'
-							)}
+							CORS must be properly configured by the provider to allow requests
 						</div>
 					</div>
 				</div>
